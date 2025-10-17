ebc9c1f446f25563cc757beee72a0f87e5a2b837
Afonso Lage <lage.afonso@gmail.com>
2025-10-17 09:50:35 -0300

    WIP: atlas example

--- crates/procgen/examples/atlas.rs
+++ crates/procgen/examples/atlas.rs
@@ -1,3 +1,43 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PresentMode};
+use bevy_egui::EguiPlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
-fn main() {}
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    present_mode: PresentMode::Immediate,
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }),))
+        .add_plugins((EguiPlugin::default(), WorldInspectorPlugin::default()))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
+    commands.spawn(Camera2d);
+
+    let image = Image { ..default() };
+
+    commands.spawn((
+        Name::new("Root"),
+        Node {
+            position_type: PositionType::Absolute,
+            width: percent(100.0),
+            height: percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![
+            (ImageNode {
+                image: images.add(image),
+                ..default()
+            })
+        ],
+    ));
+}
